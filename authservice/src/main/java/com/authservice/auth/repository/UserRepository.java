d74e6454ec39a005a9f25b46b39742082edcf0c8
Manjistha <mmanjistha@gmail.com>
2025-11-22 16:17:09 +0530

    dashboard profile forget password merged

--- authservice/src/main/java/com/authservice/auth/repository/UserRepository.java
+++ authservice/src/main/java/com/authservice/auth/repository/UserRepository.java
@@ -3,6 +3,7 @@ package com.authservice.auth.repository;
 import com.authservice.auth.model.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import java.util.Optional;
+import java.util.List;   
 
 public interface UserRepository extends MongoRepository<User, String> {
 
@@ -13,4 +14,6 @@ public interface UserRepository extends MongoRepository<User, String> {
     User findByEmail(String email);
 
     User findByResetToken(String resetToken);
+
+    List<User> findAllByEmail(String email);  
 }
