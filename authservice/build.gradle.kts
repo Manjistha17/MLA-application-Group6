bbbb5bd5d08909539fea7b5a5d8eb8be10c69365
ponni2025 <pondivya.rajesh@gmail.com>
2025-12-03 21:36:24 -0800

    commit swagger changes in docker and build gradle file

--- authservice/build.gradle.kts
+++ authservice/build.gradle.kts
@@ -20,6 +20,7 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-security")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.springframework.boot:spring-boot-starter-mail")
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.2.0")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.springframework.security:spring-security-test")
 }
