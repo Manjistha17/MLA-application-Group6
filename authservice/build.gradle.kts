671ebc178b3e8c419cee8ef709ebddecccbd1e63
ponni2025 <pondivya.rajesh@gmail.com>
2025-12-03 21:53:17 -0800

    added correct version springdoc-openapi-ui:1.7.0

--- authservice/build.gradle.kts
+++ authservice/build.gradle.kts
@@ -20,7 +20,7 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-security")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.springframework.boot:spring-boot-starter-mail")
-    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.2.0")
+	implementation("org.springdoc:springdoc-openapi-ui:1.7.0")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.springframework.security:spring-security-test")
 }
