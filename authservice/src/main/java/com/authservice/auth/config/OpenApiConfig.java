5c1ba7a6bc9831ab45fc227d71dd0ae9d5d3e220
ponni2025 <pondivya.rajesh@gmail.com>
2026-01-27 22:40:37 -0800

    added changes in openapiconfig

--- authservice/src/main/java/com/authservice/auth/config/OpenApiConfig.java
+++ authservice/src/main/java/com/authservice/auth/config/OpenApiConfig.java
@@ -14,20 +14,10 @@ public class OpenApiConfig {
 
     @Bean
     public OpenAPI authServiceOpenAPI() {
-        Server localServer = new Server();
-        localServer.setUrl("http://localhost:8080");
-        localServer.setDescription("Server URL in Development environment");
-
-        Contact contact = new Contact();
-        contact.setName("MLA Fitness App Team");
-
-        Info info = new Info()
-                .title("Auth Service API")
-                .version("1.0")
-                .contact(contact)
-                .description("API documentation for the Authentication and User Management Service. " +
-                        "This service handles user registration, login, profile management, and password reset functionality.");
-
-        return new OpenAPI().info(info).servers(List.of(localServer));
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Auth Service API")
+                        .version("1.0")
+                        .description("API documentation for the Authentication and User Management Service."));
     }
 }
