9328652047196f5fce01fd53ee144a407e2ea9c7
harithan94 <haritha22081117@gmail.com>
2026-03-03 17:22:31 +0530

    changes for notifications

--- frontend/src/components/HeroBanner.jsx
+++ frontend/src/components/HeroBanner.jsx
@@ -30,7 +30,7 @@ const HeroBanner = ({ onLogin }) => {
     setLoading(true);
 
     try {
-      const response = await axios.post("http://16.171.162.5:8080/api/auth/login", {
+      const response = await axios.post("https://d393qv373r18to.cloudfront.net/api/auth/login", {
         username,
         password,
       });
